ac158f936ec0b6bd77bc6445486d5b5b23a0edbe
Amal Krishna <amalkrishna10766@gmail.com>
2026-07-16 21:40:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -138,6 +138,7 @@ def make_chart(sig) -> io.BytesIO:
 
 # ---------------- DISCORD ----------------
 intents = discord.Intents.default()
+intents.message_content = True
 client = discord.Client(intents=intents)
 
 
